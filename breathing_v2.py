cd555b4950a79de5a519cb9ca6f5a6c55c4d6e8f
samjkenney <129895815+samjkenney@users.noreply.github.com>
2025-07-11 16:51:31 -0500

    data

--- breathing_v2.py
+++ breathing_v2.py
@@ -8,6 +8,7 @@ import time
 import csv
 import acconeer.exptool as et
 from acconeer.exptool import a121
+from acconeer.exptool.a121 import Profile
 from acconeer.exptool.a121.algo.breathing import RefApp
 from acconeer.exptool.a121.algo.breathing._ref_app import (
     BreathingProcessorConfig,
@@ -30,7 +31,7 @@ def main():
     # Default configuration is 6, 20, 20
     # Accuracy seems to be wayy far off set to 1
     breathing_processor_config = BreathingProcessorConfig(
-        lowest_breathing_rate=4,
+        lowest_breathing_rate=1,
         highest_breathing_rate=15,
         time_series_length_s=20,
     )
@@ -48,12 +49,13 @@ def main():
     ref_app_config = RefAppConfig(
         use_presence_processor=True,
         #Adjust start and end of range as appropriate
-        start_m=0.2, #cannot be 0
-        end_m=0.6,
+        start_m=0.35, #cannot be 0
+        end_m=0.7,
         num_distances_to_analyze=3,
         distance_determination_duration=5,
         breathing_config=breathing_processor_config,
         presence_config=presence_config,
+        #profile = Profile.PROFILE_4
 
     )
 
@@ -64,9 +66,9 @@ def main():
     client = a121.Client.open(serial_port="COM7", override_baudrate=115200)
     client.setup_session(sensor_config)
 
-    ratio = 0.843
-
-    with a121.H5Recorder("./raw_data-44.h5",client):
+    ratio = 1
+    
+    with a121.H5Recorder("./raw_data-54.h5",client):
         # Preparation for reference application processor
         ref_app = RefApp(client=client, sensor_id=sensor, ref_app_config=ref_app_config)
         ref_app.start()
@@ -76,11 +78,12 @@ def main():
 
         start_time = time.time()
         #opens a csv file
-        with open('sensorData-h9-d0.4-front-halfrange-v5.csv', 'w', newline = '') as csvfile:
+        with open('sensorData-h9-d.5-front-v5.csv', 'w', newline = '') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow(["Timestamp", "Breath Rate"])
             while not interrupt_handler.got_signal:
                 #Gets the data from the sensor
+                distressCounter = 0
                 processed_data = ref_app.get_next()
                 try:
                     if (processed_data.breathing_result):
@@ -91,6 +94,12 @@ def main():
                             tosend = [currentTime, calibratedBPM]
                             #Sends an array of the time and respiration rate to csv file
                             csv_writer.writerow(tosend)
+                            if (calibratedBPM < 5):
+                                distressCounter = distressCounter + 1
+                            else:
+                                distressCounter = 0
+                            if (distressCounter > 960):
+                                print("signal send")
                         else:
                             print("Calculating respiration rate...")
                     else:
